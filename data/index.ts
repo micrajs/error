0429f6b04d3d47838e513590ec297af755c4e5a1
Olavo Amorim Santos <olavo.a.santos@gmail.com>
2022-04-21 17:00:19 -0500

    feat: created normalizeError utility

--- data/index.ts
+++ data/index.ts
@@ -2,3 +2,4 @@ export * from './classes/MicraError';
 export * from './classes/ValidationError';
 export * from './classes/WrappedError';
 export * from './utilities/isMicraError';
+export * from './utilities/normalizeError';
