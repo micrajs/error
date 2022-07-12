0d92720116eb71db9fa8eed415f364007c1eb724
Olavo Amorim Santos <olavo.a.santos@gmail.com>
2022-07-11 20:58:36 -0500

    fix: exported new error classes

--- data/index.ts
+++ data/index.ts
@@ -1,3 +1,5 @@
+export * from './classes/Error';
+export * from './classes/HTTPError';
 export * from './classes/MicraError';
 export * from './classes/ValidationError';
 export * from './classes/WrappedError';
